b1ced247658f997aa6a23fa6ce24143cde341262
Vladimir <vilonva@gmail.com>
2024-06-23 14:10:31 +0300

    Добавлен Лейбл и кнопка увеличения счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    var count: Int = 0
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        counterLabel.text = "Значение счётчика: " + String(count)
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func increaseCounter(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: " + String(count)
+    }
+    
 
     
 }
